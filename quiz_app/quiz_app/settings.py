e84fa831d0ae30255f31935d7755cc0680aa8b7e
stanislawK <stanislaw.krupienko@gmail.com>
2022-06-21 19:09:35 +0200

    5 urls changes in templates.

--- quiz_app/quiz_app/settings.py
+++ quiz_app/quiz_app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "questions",
+    "questions.apps.QuestionsConfig",
 ]
 
 MIDDLEWARE = [
