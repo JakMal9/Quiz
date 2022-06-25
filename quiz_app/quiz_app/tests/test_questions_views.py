ca6e03e346efbd7f88d1777eb9321a04458b3f15
stanislawK <stanislaw.krupienko@gmail.com>
2022-06-25 20:14:06 +0200

    5 more unittests for question views.

--- quiz_app/quiz_app/tests/test_questions_views.py
+++ quiz_app/quiz_app/tests/test_questions_views.py
@@ -1,6 +1,6 @@
 import pytest
 
-from questions.models import Question, QuestionAnswer
+from questions.models import Answer, Question, QuestionAnswer
 
 
 @pytest.mark.django_db
@@ -19,6 +19,13 @@ def test_question_details_view(client, question_with_answers):
     assert question_db.content in res.content.decode("utf-8")
     for answer in question_db.answers.all():
         assert answer.content in res.content.decode("utf-8")
+
+
+@pytest.mark.django_db
+def test_question_details_view_index_out_of_range(client, question_with_answers):
+    question_last_id = Question.objects.last().pk
+    res = client.get(f"/questions/{question_last_id + 1}/")
+    assert res.status_code == 404
 
 
 @pytest.mark.django_db
@@ -43,3 +50,25 @@ def test_answer_view(client, question_with_answers, content_type):
                 "Success! Your answer is correct!" if qa.correct else "Try again"
             )
             assert expected_msg in res.content.decode("utf-8")
+
+
+@pytest.mark.parametrize("method", ["get", "put", "patch", "delete"])
+def test_answer_view_method_not_allowed(client, method):
+    res = getattr(client, method)(f"/questions/1/answer/")
+    assert res.status_code == 405
+
+
+@pytest.mark.django_db
+def test_answer_view_index_out_of_range(client, question_with_answers):
+    last_question_id = Question.objects.last().pk
+    last_answer_id = Answer.objects.last().pk
+    out_of_range_q_id = last_question_id + 1
+    out_of_range_a_id = last_answer_id + 1
+    res = client.post(
+        f"/questions/{out_of_range_q_id}/answer/", {"answer": last_answer_id}
+    )
+    assert res.status_code == 404
+    res = client.post(
+        f"/questions/{last_question_id}/answer/", {"answer": out_of_range_a_id}
+    )
+    assert res.status_code == 404
