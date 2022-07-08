89263aa8ca6afc04dd8605cc0e0cf6a9ba957879
Stanislaw <32984705+stanislawK@users.noreply.github.com>
2022-07-08 11:27:40 +0200

    [ resolves #5 ] questions models and views added. (#15)
    * 5 questions models and views added.
    
    * 5 urls changes in templates.
    
    * 5 checking correct answer with js.
    
    * 5 check correct answer frontend
    
    * 5 switch beteewn JS and HTML form.
    
    * 5 more unittests for question views.
    
    * 5 admin view improvements

--- quiz_app/quiz_app/tests/test_models.py
+++ quiz_app/quiz_app/tests/test_models.py
@@ -1,8 +1,27 @@
 import pytest
 from django.contrib.auth.models import User
 
+from questions.models import Answer, Question, QuestionAnswer
+
 
 @pytest.mark.django_db
 def test_user_create():
     User.objects.create_user("john", "johndoe@test.com", "StrongPassword")
     assert User.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_create_new_question_with_answers(question, answers):
+    new_question = Question.objects.create(**question)
+    for answer in answers:
+        content = answer["content"]
+        new_question.answers.create(
+            content=content, through_defaults={"correct": content == "Cinderella"}
+        )
+    assert Question.objects.count() == 1
+    assert Answer.objects.count() == QuestionAnswer.objects.count() == 4
+    question = Question.objects.first()
+    assert question.answers.count() == 4
+    correct_answer = question.answers.filter(questionanswer__correct=True)
+    assert len(correct_answer) == 1
+    assert correct_answer[0].content == "Cinderella"
